539b142a35ee9b5a6901cfd981203a918dd0f049
7hayMuller <thaynamuller88@gmail.com>
2021-04-29 18:04:30 -0300

    Adding layout to New Contact screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:contact_book/helpers/contact_helper.dart';
+import 'package:contact_book/ui/contact_page.dart';
 import 'package:contact_book/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:HomePage(),
-
+    home:ContactPage(),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
